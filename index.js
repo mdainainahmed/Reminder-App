f3a32a9eb2cc92c6c53ad9d51696ceb3032410c0
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-08-14 16:27:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+document.querySelectorAll("button").forEach((element) => {
+  element.addEventListener("click", setReminder);
+});
+
+function setReminder() {
+  const selectedDay = document.getElementById("daySelector").value;
+  const selectedTime = document.getElementById("timeSelector").value;
+  const selectedActivity = document.getElementById("activitySelector").value;
+
+  const currentTime = new Date();
+  const reminderTime = new Date(
+    currentTime.toDateString() + " " + selectedTime
+  );
+
+  if (reminderTime < currentTime) {
+    reminderTime.setDate(reminderTime.getDate() + 1);
+  }
+
+  const timeUntilReminder = reminderTime - currentTime;
+
+  setTimeout(() => {
+    playSound();
+    alert(`Time for ${selectedActivity} on ${selectedDay}`);
+  }, timeUntilReminder);
+}
+
+function playSound() {
+  const audio = new Audio("music/music.mp3");
+  audio.play();
+}
