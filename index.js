4cce192b44c1d3262d77099809b9956cfc0f7b58
Md Ainain Ahmed <108012966+mdainainahmed@users.noreply.github.com>
2023-08-14 17:07:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,36 @@ document.querySelectorAll("button").forEach((element) => {
   element.addEventListener("click", setReminder);
 });
 
+function getDayName(date) {
+  const options = { weekday: "long" };
+  return new Intl.DateTimeFormat("en-US", options).format(date);
+}
+
 function setReminder() {
   const selectedDay = document.getElementById("daySelector").value;
   const selectedTime = document.getElementById("timeSelector").value;
   const selectedActivity = document.getElementById("activitySelector").value;
 
-  const currentTime = new Date();
-  const reminderTime = new Date(
-    currentTime.toDateString() + " " + selectedTime
-  );
+  const currentDate = new Date();
+  const dayName = getDayName(currentDate);
 
-  if (reminderTime < currentTime) {
-    reminderTime.setDate(reminderTime.getDate() + 1);
+  if (selectedDay === dayName) {
+    const currentTime = new Date();
+    const reminderTime = new Date(
+      currentTime.toDateString() + " " + selectedTime
+    );
+
+    if (reminderTime < currentTime) {
+      reminderTime.setDate(reminderTime.getDate() + 1);
+    }
+
+    const timeUntilReminder = reminderTime - currentTime;
+
+    setTimeout(() => {
+      playSound();
+      alert(`Time for ${selectedActivity} on ${selectedDay}`);
+    }, timeUntilReminder);
   }
-
-  const timeUntilReminder = reminderTime - currentTime;
-
-  setTimeout(() => {
-    playSound();
-    alert(`Time for ${selectedActivity} on ${selectedDay}`);
-  }, timeUntilReminder);
 }
 
 function playSound() {
